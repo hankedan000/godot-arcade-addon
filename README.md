674b30cf67b6f5c24b119b033f1aaa82eec2f089
hankedan000 <dany32412@gmail.com>
2025-05-31 13:14:44 -0400

    Moved utils to gd_basics repo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # godot-arcade-addon
 Addons for Godot games targeting my arcade machine
+
+# Dependencies
+ * requires [gd_basics](https://github.com/hankedan000/godot-basics-addon)
--- scripts/dict_utils.gd
+++
@@ -1,7 +0,0 @@
-class_name DictUtils
-extends Object
-
-static func get_w_default(dict: Dictionary, key: Variant, default_value=null):
-	if dict.has(key):
-		return dict[key]
-	return default_value
